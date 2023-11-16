244cb3666ef09753b7395afd4e6914f87232c552
cmong0516 <cmong0516@gmail.com>
2023-11-17 01:38:10 +0900

    2023.11.17 item3

--- src/item2/Calzone.java
+++ src/item2/Calzone.java
@@ -1,15 +1,16 @@
 package item2;
 
-public class Calzone extends Pizza{
+public class Calzone extends Pizza {
     private final boolean sauceInside;
 
     public static class Builder extends Pizza.Builder<Builder> {
-        private boolean sauceInside =false;
+        private boolean sauceInside = false;
 
         public Builder sauceInside() {
             sauceInside = true;
             return this;
         }
+
         @Override
         public Calzone build() {
             return new Calzone(this);
---
+++ src/item3/Singleton3.java
@@ -0,0 +1,9 @@
+package item3;
+
+public enum Singleton3 {
+    INSTANCE;
+
+    public void run() {
+        System.out.println("싱글톤 3");
+    }
+}
