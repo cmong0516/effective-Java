8b2e0c0da038a98f0e23a1862266b9deb09acdb4
cmong0516 <cmong0516@gmail.com>
2023-11-21 12:05:15 +0900

    2023.11.21 item23

--- src/item23/Item23.java
+++ src/item23/Item23.java
@@ -3,5 +3,14 @@ package item23;
 public class Item23 {
     // 태그달린 클래스 보다는 클래스 계층구조를 활용하라.
 
+    public static void main(String[] args) {
+        Circle circle = new Circle(4);
+        System.out.println("circle.area() = " + circle.area());
 
+        Rectangle rectangle = new Rectangle(10, 4);
+        System.out.println("rectangle.area() = " + rectangle.area());
+
+        Square square = new Square(10);
+        System.out.println("square.area() = " + square.area());
+    }
 }
