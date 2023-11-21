78bee936d43964e1b017ade8324d2a5a29538189
cmong0516 <cmong0516@gmail.com>
2023-11-21 12:03:50 +0900

    2023.11.21 item23

---
+++ src/item23/Figure2.java
@@ -0,0 +1,40 @@
+package item23;
+
+abstract class Figure2 {
+    abstract double area();
+}
+
+class Circle extends Figure2 {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * (radius * radius);
+    }
+}
+
+class Rectangle extends Figure2 {
+    private final double length;
+    private final double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double area() {
+        return length * width;
+    }
+}
+
+class Square extends Rectangle {
+
+    public Square(double side) {
+        super(side, side);
+    }
+}
---
+++ src/item23/Item23.java
@@ -0,0 +1,7 @@
+package item23;
+
+public class Item23 {
+    // 태그달린 클래스 보다는 클래스 계층구조를 활용하라.
+
+
+}
